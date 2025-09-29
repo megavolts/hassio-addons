d5b86ac5c67e66fea45ebdb56bc97725bcdc6196
Marc Oggier <marc.oggier@megavolts.ch>
2025-09-29 11:50:29 -0800

    Added creation of venv with smbus2

--- i2c-sensor/rootfs/run.sh
+++ i2c-sensor/rootfs/run.sh
@@ -2,6 +2,15 @@
 
 PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
+echo "Create virtual environment"
+mkdir /virtual_env
+python3 -m venv /virtual_env
+source /virtual_env/bin/activate
+
+echo "Install packages"
+python3 -m pip install smbus2
+python3 -m pip install paho-mqtt
+
 echo "I2C test starting..."
 python3 /sensor/i2c-test.py
 
